daf7cfc7c4f60cebac294042670f34d9b38e91b4
FlaviaSoreca <102346681+FlaviaSo@users.noreply.github.com>
2022-10-10 19:50:56 +0200

    Update 02-explore-REPL.js

--- 02-explore-REPL.js
+++ 02-explore-REPL.js
@@ -1,3 +1,3 @@
 // Use the Node.js REPL to list the methods provided by the Node.js core crypto module. Use one of these methods to generate a random ID.
 
-// Nel terminale -> crypto.random()
+// Nel terminale -> crypto.randomUUID()
